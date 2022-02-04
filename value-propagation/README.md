17182b27aee7a5e0076ef42c292924b9775bd169
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-02-04 17:50:02 -0600

    Add some value propogation tests

---
+++ value-propagation/README.md
@@ -0,0 +1,31 @@
+### Value propagation testing
+
+> for every attribute of every built-in parameter handler (e.g. cutoff, v-site distance, bond length): an OpenMM system created using openff-2.0.0.offxml but with the attribute of the handler perturbed yields an OpenMM system with that value also modified, again to ensure that changes to parameter attributes are actually captured and its not just the default values that get passed through
+
+In most cases, only the value of one attribute needs to be changed at a time. The propagation of these changed values can be tested by comparing the output OpenMM systems, written as XML, and compared via deepdiff
+
+For each of the below attributes, tests ensure that
+* modifying their values produces an `openmm.System` that differs from the original
+* the corresponding value(s) in the `openmm.System` match the modified value(s)
+
+* `vdWHandler.cutoff`
+* `vdWHandler.scale14`
+* `Constraint.distance`
+
+For each of the below attributes, the OpenFF Toolkit versoin 0.10.x only supports one value and
+therefore no value propagation can be tested:
+* `vdWHandler.scale12`
+* `vdWHandler.scale13`
+* `vdWHandler.scale15`
+* `vdWHandler.potential`
+* `vdWHandler.combining_rules`
+* `vdWHandler.method`
+* `BondHandler.potential`
+* `BondHandler.fractional_bondorder_method`
+* `BondHandler.fractional_bondorder_interpolation`
+* `AngleHandler.potential`
+* `ProperTorsionHandler.potential`
+* `ProperTorsionHandler.fractional_bondorder_method`
+* `ProperTorsionHandler.default_idivf`
+* `ImproperTorsionHandler.potential`
+* `ImproperTorsionHandler.default_idivf`
---
+++ value-propagation/test_modifications.py
@@ -0,0 +1,80 @@
+import openmm
+import pytest
+import xmltodict
+from deepdiff import DeepDiff
+from openff.toolkit.tests.create_molecules import create_ethanol
+from openff.toolkit.tests.utils import get_14_scaling_factors
+from openff.toolkit.typing.engines.smirnoff import ForceField
+from openff.units import unit
+from openmm import unit as openmm_unit
+
+
+def postprocessor(path, key, value):
+    try:
+        return key, int(value) if "." not in value else float(value)
+    except (ValueError, TypeError):
+        return key, value
+
+
+def deep_diff(system1: openmm.System, system2: openmm.System):
+    dict1 = xmltodict.parse(
+        openmm.XmlSerializer.serialize(system1), postprocessor=postprocessor
+    )
+    dict2 = xmltodict.parse(
+        openmm.XmlSerializer.serialize(system2), postprocessor=postprocessor
+    )
+
+    diff = DeepDiff(dict1, dict2, ignore_order=True, significant_digits=6)
+
+    return diff
+
+
+class TestValuePropagation:
+    @pytest.fixture()
+    def sage(self):
+        return ForceField("openff-2.0.0.offxml")
+
+    @pytest.fixture()
+    def ethanol_topology(self):
+        return create_ethanol().to_topology()
+
+
+class TestvdW(TestValuePropagation):
+    def test_modify_cutoff(self, sage, ethanol_topology):
+        original_system = sage.create_openmm_system(ethanol_topology)
+        sage["vdW"].cutoff = 1.23456 * unit.nanometer
+        modified_system = sage.create_openmm_system(ethanol_topology)
+
+        assert len(deep_diff(original_system, modified_system)) > 0
+
+        for force in modified_system.getForces():
+            if isinstance(force, openmm.NonbondedForce):
+                assert force.getCutoffDistance() == 1.23456 * openmm_unit.nanometer
+
+    def test_modify_scale_14(self, sage, ethanol_topology):
+        original_system = sage.create_openmm_system(ethanol_topology)
+        sage["vdW"].scale14 = 0.123456
+        modified_system = sage.create_openmm_system(ethanol_topology)
+
+        assert len(deep_diff(original_system, modified_system)) > 0
+
+        _, found_scale_14 = get_14_scaling_factors(modified_system)
+
+        assert all(val == pytest.approx(0.123456) for val in found_scale_14)
+
+
+class TestConstraints(TestValuePropagation):
+    def test_modify_constraint_distance(self, sage, ethanol_topology):
+        original_system = sage.create_openmm_system(ethanol_topology)
+        sage["Constraints"].parameters[0].distance = 1.23456 * unit.angstrom
+        modified_system = sage.create_openmm_system(ethanol_topology)
+
+        assert len(deep_diff(original_system, modified_system)) > 0
+
+        for constraint_index in range(modified_system.getNumConstraints()):
+            _, _, constraint_distance = modified_system.getConstraintParameters(
+                constraint_index
+            )
+            assert constraint_distance.value_in_unit(
+                openmm_unit.angstrom
+            ) == pytest.approx(1.23456)
